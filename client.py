de003a5242ae30506cf492264759f4defcedfd3c
briscalon <brian_scanlon@hotmail.co.uk>
2022-11-21 22:34:42 +0000

    Modified code to include shared states. Client now looks more like server and can handle state machine.  Currently a bug where the client is waiting a response, but the server is awaiting a connection on line 124

--- client.py
+++ client.py
@@ -1,23 +1,59 @@
 import socket
-# create our connection socket, IPv4 and streaming connection
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-play = True
-user_doing_stuff = True
 
-# connect to the server
-s.connect(("127.0.0.1", 50001))
+class Client:
+    def __init__(self, host="127.0.0.1", port=50000):
+        self.HOST = host
+        self.PORT = port
 
-while play:
-    # set the buffer size to 1024 and decode to UTF-8 and print the message
-    message_received = s.recv(1024)
-    print(message_received.decode("utf-8"))
-    message_send = input("Type option and press enter: ").upper()
-    if message_send == "QUIT":
-        play = False
-        # shutdown socket connection to server
-        s.shutdown(socket.SHUT_RDWR)
-        break
-    else:
-        s.sendall(message_send.encode())
+    def run(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.HOST, self.PORT))
 
+            while True:
+                message = input("Please enter a command: ")
+                s.sendall(message.encode("UTF-8"))
 
+                data = s.recv(1024)
+                print(f"Received {data!r}")
+
+                response = data.decode("utf-8")
+
+                if response.startswith("COMPLEXTEST"):
+                    print("Entering a different style of state")
+                    while True:
+                        message = input("Please enter a message: ")
+                        s.sendall(message.encode())
+
+                        if message == "TERMINATE":
+                            data = s.recv(1024)
+                            print(f"Received {data!r}")
+                            break
+                if message == "Quit":
+                    s.shutdown(socket.SHUT_RDWR)
+                    break
+
+if __name__ == "__main__":
+    client = Client("127.0.0.1", 50001)
+    client.run()
+# # create our connection socket, IPv4 and streaming connection
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# play = True
+# user_doing_stuff = True
+#
+# # connect to the server
+# s.connect(("127.0.0.1", 50001))
+#
+# while play:
+#     # set the buffer size to 1024 and decode to UTF-8 and print the message
+#     message_received = s.recv(1024)
+#     print(message_received.decode("utf-8"))
+#     message_send = input("Type option and press enter: ").upper()
+#     if message_send == "QUIT":
+#         play = False
+#         # shutdown socket connection to server
+#         s.shutdown(socket.SHUT_RDWR)
+#         break
+#     else:
+#         s.sendall(message_send.encode())
+#
+#
--- server.py
+++ server.py
@@ -2,15 +2,15 @@ import socket
 import gamefunctions
 from enum import Enum
 
+
 class State(Enum):
     Start = 1
     Login = 2
     Menus = 3
     Play = 4
+    Complex = 5
     Error = 99
 
-
-unameReceived = False
 
 # def readFile(name):
 #     dataLine = name.readLine()
@@ -18,7 +18,8 @@ unameReceived = False
 
 
 class Server:
-    def __init__(self, host, port):
+
+    def __init__(self, host="127.0.0.1", port=50000):
         # define network parameters
         self.HOST = host
         self.PORT = port
@@ -26,22 +27,42 @@ class Server:
         self.previousState = None
         self.running = True
         self.counter = 0
+        self.messages = []
 
+        self.client_connection = None
+        self.addr = None
 
     def start(self, message):
         if message.startswith("LOGIN"):
             self.previousState = self.currentState
             self.currentState = State.Login
             message = "Login Menu Loading..."
-        # elif message.startswith("Login"):
-        #     self.previousState = self.currentState
-        #     self.currentState = State.Login
-        #     message = "Moving to Login"
         else:
             message = "You must type in Login and press enter"
         return message
 
-    def login(self, message, unameReceived):
+    def complex(self, message):
+        while True:
+            data = self.client_connection.recv(1024)
+            if not data:
+                self.running = False
+                message = "Error"
+                break
+
+            message = data.decode("utf-8")
+            print(message)
+            # Handle termination
+            if message == "TERMINATE":
+                message = "".join(self.messages)
+                break
+            else:
+                self.messages.append(message)
+
+        self.currentState = State.Start
+        self.previousState = State.Complex
+        return message
+
+    def login(self, message, unameReceived=None):
         # while unameReceived is false, skip asking for the password and ask for the username instead
         # when unameReceived is true, ask for the password.
         if unameReceived:
@@ -56,19 +77,30 @@ class Server:
             if message.startswith("ADMIN"):
                 unameReceived = True
                 message = "Password?"
-            message = "Echoing: " + message + str(self.currentState)
-        return message, unameReceived
-
+        return message
 
     def menus(self, message):
-        if message.startswith("Echo"):
+        if message.startswith("Menus"):
             self.previousState = self.currentState
-            self.currentState = State.Echo
-            message = "Moving to Echo"
+            self.currentState = State.Menus
+            message = "Moving to Menus"
+        return message
+
+    def play(self, message):
+        if message.startswith("Play"):
+            self.previousState = self.currentState
+            self.currentState = State.Play
+            message = "Moving to Play"
+        return message
+
+    def handleError(self, message):
+        message = "An Error has occurred, returning to start state"
+        self.currentState = State.Start
+        self.previousState = State.Error
         return message
 
     def run(self):
-        play = True
+        # play = True
         # define socket object
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
@@ -78,53 +110,39 @@ class Server:
             s.bind((self.HOST, self.PORT))
             # listen on the socket for a connection
             s.listen()
+            self.client_connection, self.addr = s.accept()
 
-            # accept a client connection define the client socket and address
-            client_connection, address = s.accept()
-            print(f"connection established with {address}.")
-            client_connection.send(bytes("Welcome to the Game Server\n**************************\n\nPlease type Login and press enter to log in to the server", "utf-8"))
+            with self.client_connection:
+                # accept a client connection define the client socket and address
+                print(f"connection established with {self.addr}.")
+                self.client_connection.send(bytes("Welcome to the Game Server.  Please type Login and press enter to log in to the server", "utf-8"))
 
-            while play:
-                data = client_connection.recv(1024)
-                if not data:
-                    break
-                # Do some processing
-                message = data.decode().upper()
-                # Handle termination
-                if message == "Quit":
-                    message = "Acknowledge quitting"
-                    self.running = False
-                else:
-                    # State machines cover the following:
-                    #
-                    if self.currentState == State.Start:
-                        message = self.start(message)
-                    elif self.currentState == State.Login:
-
-                        message, unameReceived = self.login(message, unameReceived)
-                    elif self.currentState == State.Menus:
-                        message = self.menus(message)
+                while self.running:
+                    if self.currentState == State.Complex:
+                        message = self.complex(message)
                     else:
-                        try:
-                            cmd, arg = message.split(" ", 1)
-                        except ValueError:
-                            cmd = "Error"
+                        data = self.client_connection.recv(1024)
+                        if not data:
+                            break
+                        message = data.decode("utf-8")
 
-                        try:
-                            value = int(arg)
-                        except ValueError:
-                            value = 0
-                        message = str(self.counter)
+                        if message == "Quit":
+                            message = "Acknowledge quitting"
+                            self.running = False
+                        else:
+                            if self.currentState == State.Start:
+                                message = self.start(message)
+                            elif self.currentState == State.Login:
+                                message = self.login(message)
+                            elif self.currentState == State.Menus:
+                                message = self.menus(message)
+                            elif self.currentState == State.Play:
+                                message = self.play(message)
+                            else:
+                                message = self.handleError(message)
+                self.client_connection.sendall(message.encode("utf-8"))
 
-                    # else:
-                    #     message = "An Error has occurred, returning to start state"
-                    #     self.currentState = State.Start
-                    #     self.previousState = State.Error
-
-                # send back the response
-                client_connection.sendall(message.encode("utf-8"))
 
 if __name__ == "__main__":
     server = Server("127.0.0.1", 50001)
     server.run()
-
